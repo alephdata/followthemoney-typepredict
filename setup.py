16cf239112ad65a8243b2a19cd4f1298ca080318
Micha Gorelick <mynameisfiber@gmail.com>
2021-03-15 15:12:24 +0100

    Bump version: 0.0.2 → 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="followthemoney-typepredict",
-    version="0.0.2",
+    version="0.1.0",
     long_description="Followthemoney Type Prediction",
     classifiers=[
         "Development Status :: 3 - Alpha",
